c3e87637e925a6e99724ff1f5b2873e371212e55
Miguel Olmedo <miguel.olmedo@globant.com>
2021-03-02 14:42:28 -0600

    [Feat] Add project and SPM

---
+++ Uala/Uala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uala
+//
+//  Created by Miguel Angel Olmedo Perez on 02/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
